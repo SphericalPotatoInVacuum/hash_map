46da181dccf06b469198736d2a2237bdfa3e08e1
Aleksandr <a@bogdanov.co>
2020-03-08 03:09:58 +0300

    Renamed data_ and pdata_ to buckets_ and dict_ accordingly

--- hash_map.h
+++ hash_map.h
@@ -15,14 +15,14 @@ class HashMap {
   using list_iterator = typename std::list<iterator>::iterator;
 
   explicit HashMap(Hash hash = Hash()) : hash_function_(hash) {
-    data_.resize(kMinSize);
-    dsize_ = kMinSize;
+    buckets_.resize(kMinSize);
+    buckets_size_ = kMinSize;
   }
 
   template <class T>
   HashMap(T begin, T end, Hash hash = Hash()) : hash_function_(hash) {
-    dsize_ = std::distance(begin, end);
-    data_.resize(dsize_);
+    buckets_size_ = std::distance(begin, end);
+    buckets_.resize(buckets_size_);
     for (T it = begin; it != end; ++it) {
       insert({it->first, it->second});
     }
@@ -30,7 +30,7 @@ class HashMap {
 
   HashMap &operator=(HashMap hm) {
     clear();
-    for (auto it : hm.pdata_) {
+    for (auto it : hm.dict_) {
       insert(std::make_pair(it.first, it.second));
     }
     return *this;
@@ -49,26 +49,26 @@ class HashMap {
 
   const ValueType &at(KeyType key) const {
     const_iterator it = find(key);
-    if (it == pdata_.end()) {
+    if (it == dict_.end()) {
       throw std::out_of_range("blah");
     }
     return it->second;
   }
 
   void clear() {
-    pdata_.clear();
-    pdsize_ = 0;
-    data_.clear();
-    data_.resize(kMinSize);
-    dsize_ = kMinSize;
+    dict_.clear();
+    dict_size_ = 0;
+    buckets_.clear();
+    buckets_.resize(kMinSize);
+    buckets_size_ = kMinSize;
   }
 
   const size_t size() const {
-    return pdsize_;
+    return dict_size_;
   }
 
   const bool empty() const {
-    return pdsize_ == 0;
+    return dict_size_ == 0;
   }
 
   const Hash hash_function() const {
@@ -78,12 +78,12 @@ class HashMap {
   void insert(std::pair<KeyType, ValueType> element) {
     auto p = find_iterator(element.first);
 
-    if (p.second == data_[p.first].end()) {
-      iterator f = pdata_.insert(pdata_.end(),
-                                 std::make_pair(element.first, element.second));
-      data_[p.first].insert(data_[p.first].end(), f);
-      ++pdsize_;
-      if (dsize_ == pdsize_) {
+    if (p.second == buckets_[p.first].end()) {
+      iterator f = dict_.insert(dict_.end(),
+                                std::make_pair(element.first, element.second));
+      buckets_[p.first].insert(buckets_[p.first].end(), f);
+      ++dict_size_;
+      if (buckets_size_ == dict_size_) {
         rebuild();
       }
     }
@@ -91,19 +91,19 @@ class HashMap {
 
   void erase(KeyType key) {
     auto p = find_iterator(key);
-    if (p.second != data_[p.first].end()) {
-      pdata_.erase(*p.second);
-      data_[p.first].erase(p.second);
-      --pdsize_;
+    if (p.second != buckets_[p.first].end()) {
+      dict_.erase(*p.second);
+      buckets_[p.first].erase(p.second);
+      --dict_size_;
     }
   }
 
   iterator find(KeyType key) {
     auto p = find_iterator(key);
-    if (p.second != data_[p.first].end()) {
+    if (p.second != buckets_[p.first].end()) {
       return *p.second;
     }
-    return pdata_.end();
+    return dict_.end();
   }
 
   const_iterator find(KeyType key) const {
@@ -111,28 +111,28 @@ class HashMap {
   }
 
   iterator begin() {
-    return pdata_.begin();
+    return dict_.begin();
   }
 
   iterator end() {
-    return pdata_.end();
+    return dict_.end();
   }
 
   const_iterator begin() const {
-    return pdata_.begin();
+    return dict_.begin();
   }
 
   const_iterator end() const {
-    return pdata_.end();
+    return dict_.end();
   }
 
  private:
   void rebuild() {
-    std::vector<std::list<iterator>> new_data_(data_.size() * 2),
-        old_data_(data_);
-    dsize_ *= 2;
-    data_ = new_data_;
-    for (auto it : old_data_) {
+    std::vector<std::list<iterator>> new_buckets_(buckets_.size() * 2),
+        old_buckets_(buckets_);
+    buckets_size_ *= 2;
+    buckets_ = new_buckets_;
+    for (auto it : old_buckets_) {
       for (auto it2 : it) {
         insert_existing(it2);
       }
@@ -140,26 +140,26 @@ class HashMap {
   }
 
   void insert_existing(iterator iter) {
-    int index = hash_function_(iter->first) % data_.size();
-    data_[index].insert(data_[index].end(), iter);
-    if (pdsize_ == dsize_) {
+    int index = hash_function_(iter->first) % buckets_.size();
+    buckets_[index].insert(buckets_[index].end(), iter);
+    if (dict_size_ == buckets_size_) {
       rebuild();
     }
   }
 
   std::pair<int, list_iterator> find_iterator(KeyType key) {
-    int index = hash_function_(key) % dsize_;
+    int index = hash_function_(key) % buckets_size_;
     list_iterator it =
-        std::find_if(data_[index].begin(), data_[index].end(),
+        std::find_if(buckets_[index].begin(), buckets_[index].end(),
                      [&key](auto it) { return it->first == key; });
     return std::make_pair(index, it);
   }
 
   Hash hash_function_;
-  std::vector<std::list<iterator>> data_;
-  std::list<std::pair<const KeyType, ValueType>> pdata_;
-  size_t dsize_;
-  size_t pdsize_ = 0;
+  std::vector<std::list<iterator>> buckets_;
+  std::list<std::pair<const KeyType, ValueType>> dict_;
+  size_t buckets_size_;
+  size_t dict_size_ = 0;
 
   const size_t kMinSize = 16;
 };
